2a4026aeb1645c3e19dcc76e92a37c1acdaf18be
Tim Bryan <tim.bryan@drpgroup.com>
2022-10-04 10:43:02 +0100

    create initial spec

---
+++ src/uuidv4.test.js
@@ -0,0 +1,9 @@
+describe('uuidV4', () => {
+  it.todo('should return an array with a string by default')
+
+  it.todo('should return an string whose pattern matches the uuidv4 spec')
+
+  it.todo('should return an array of strings if a quantity param is specified')
+
+  it.todo('should return an error if the quantity param is not valid')
+})
