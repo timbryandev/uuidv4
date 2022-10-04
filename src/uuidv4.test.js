41d0ecaa7ae0adf7e4f25815bc76358bbd20fe1b
Tim Bryan <tim.bryan@drpgroup.com>
2022-10-04 11:03:04 +0100

    build tests

--- src/uuidv4.test.js
+++ src/uuidv4.test.js
@@ -1,9 +1,34 @@
+const specRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+
 describe('uuidV4', () => {
-  it.todo('should return an array with a string by default')
+  it('should return an array with a string by default', () => {
+    const idsArray = generate()
 
-  it.todo('should return an string whose pattern matches the uuidv4 spec')
+    expect(Array.isArray(idsArray)).toBe(true)
 
-  it.todo('should return an array of strings if a quantity param is specified')
+    expect(idsArray.length).toBe(1)
 
-  it.todo('should return an error if the quantity param is not valid')
+    expect(typeof idsArray[0]).toBe('string')
+  })
+
+  it('should return an string whose pattern matches the uuidv4 spec', () => {
+    const [id] = generate()
+    const matchesSpec = specRegex.test(id)
+
+    expect(matchesSpec).toBe(true)
+  })
+
+  it('should return an array of strings if a quantity param is specified', () => {
+    const idsArray = generate(5)
+
+    expect(idsArray.length).toBe(5)
+  })
+
+  it('should return an error if the quantity param is not valid', () => {
+    try {
+      generate(-1)
+    } catch (error) {
+      expect(error).toBeInstanceOf(TypeError)
+    }
+  })
 })
